1743f35bf7c0553d291643dc0c883f995c9e43ce
alrhazes <anuuman2@gmail.com>
2024-08-12 09:47:33 +0100

    commit

---
+++ src/main/java/com/example/uduth_ambulance_service/UduthAmbulanceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.uduth_ambulance_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UduthAmbulanceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UduthAmbulanceServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/uduth_ambulance_service/UduthAmbulanceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uduth_ambulance_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UduthAmbulanceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
